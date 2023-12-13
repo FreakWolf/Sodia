90d3b76cf21619f24fd0e7717444d5d57a794f06
FreakWolf <rohitskt115@gmail.com>
2023-12-13 14:32:55 +0530

    Sodia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("androidx.multidex:multidex:2.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/sodia/MainActivity.java
+++ app/src/main/java/com/example/sodia/MainActivity.java
@@ -1,17 +1,141 @@
 package com.example.sodia;
 
-// MainActivity.java
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.sodia.adapter.ViewPagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    ViewPagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        TextView textView = new TextView(this);
-        textView.setText("Hello, World!");
-        setContentView(textView);
+        init();
+
+        addTabs();
+
+    }
+
+    private void init() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+    }
+
+    private void addTabs(){
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_search));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_add));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_heart));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_heart_fill));
+
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_fill);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                switch (tab.getPosition()){
+
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_fill);
+                        break;
+
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_search);
+                        break;
+
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_add);
+                        break;
+
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_heart_fill);
+                        break;
+
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(android.R.drawable.ic_menu_help);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+                switch (tab.getPosition()){
+
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home);
+                        break;
+
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_search);
+                        break;
+
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_add);
+                        break;
+
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_heart);
+                        break;
+
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_heart_fill);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+                switch (tab.getPosition()){
+
+                    case 0:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_fill);
+                        break;
+
+                    case 1:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_search);
+                        break;
+
+                    case 2:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_add);
+                        break;
+
+                    case 3:
+                        tabLayout.getTabAt(0).setIcon(R.drawable.ic_heart_fill);
+                        break;
+
+                    case 4:
+                        tabLayout.getTabAt(0).setIcon(android.R.drawable.ic_menu_help);
+                        break;
+                }
+
+            }
+        });
     }
 }
